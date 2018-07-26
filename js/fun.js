538828c71f0f7a702395399c91ef9a965f973496
Thinh Lam <thinh851@hawaii.edu>
2018-07-25 19:46:27 -1000

    improvements on code

--- js/fun.js
+++ js/fun.js
@@ -1,4 +1,26 @@
 var createCalendar = function (){
+  var par = String(document.getElementById('txtStartDate').value).split('-');
+  var StartDate = par[0] + par[1]-1 + par[2];
+  var par = String(document.getElementById('txtEndDate').value).split('-');
+  var EndDate = par[0] + par[1]-1 + par[2];
+  var startHours = String(document.getElementById('txtStartTimeHours').value);
+  if(startHours<10) {
+      startHours = '0'+startHours
+  }
+  var startMinutes = String(document.getElementById('txtStartTimeMinutes').value);
+  if(startMinutes<10) {
+      startMinutes = '0'+startMinutes
+  }
+  var StartTime = startHours + startMinutes + '00';
+  var endHours = String(document.getElementById('txtEndTimeHours').value);
+  if(endHours<10) {
+      endHours = '0'+endHours
+  }
+  var endMinutes = String(document.getElementById('txtEndTimeMinutes').value);
+  if(endMinutes<10) {
+      endMinutes = '0'+endMinutes
+  }
+  var EndTime = endHours + endMinutes + '00';
   var SEPARATOR = (navigator.appVersion.indexOf('Win') !== -1) ? '\r\n' : '\n';
   var calendarStart = [
       'BEGIN:VCALENDAR',
@@ -36,8 +58,6 @@ var getNow = function(){
   today = mm + '/' + dd + '/' + yyyy;
   document.getElementById('txtStartDate').value = today;
   document.getElementById('txtEndDate').value = today;
-  document.getElementById('txtStartTimeHours').placeholder = hour;
-  document.getElementById('txtStartTimeMinutes').placeholder = minute;
-  document.getElementById('txtEndTimeHours').placeholder = hour;
-  document.getElementById('txtEndTimeMinutes').placeholder = minute + 1;
+  document.getElementById('txtStartTimeHours').placeholder = hour + ": " + minute;
+  document.getElementById('txtEndTimeHours').placeholder = hour + ": " + minute;
 }
